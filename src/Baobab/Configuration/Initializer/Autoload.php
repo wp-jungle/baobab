638a82a4637545550d5d93d7982254eba83f889e
Vincent Mimoun-Prat <vpratfr@gmail.com>
2015-03-18 17:39:54 +0100

    Allow automatically instantiating classes after theme setup

--- src/Baobab/Configuration/Initializer/Autoload.php
+++ src/Baobab/Configuration/Initializer/Autoload.php
@@ -5,6 +5,7 @@ namespace Baobab\Configuration\Initializer;
 use Baobab\Blade\Extension;
 use Baobab\Blade\WordPressLoopExtension;
 use Baobab\Facade\Baobab;
+use Baobab\Helper\Hooks;
 use Baobab\Helper\Paths;
 use Baobab\Helper\Strings;
 use Baobab\Theme\BaobabTheme;
@@ -22,17 +23,104 @@ class Autoload extends AbstractInitializer
     /**
      * Constructor
      *
-     * @param array $data The configuration key/value pairs
+     * @param string $id   The ID of the initializer
+     * @param array  $data The configuration key/value pairs
      */
-    public function __construct($data)
+    public function __construct($id, $data)
     {
-        parent::__construct($data);
+        parent::__construct($id, $data);
+
+        $this->loadPsr4();
+        Hooks::action('after_setup_theme', $this, 'instantiateClasses');
+    }
+
+    /**
+     * Autoload all the classes inside the PSR4 sections for our groups
+     */
+    public function loadPsr4()
+    {
+        $data = $this->getData();
+
+        // Always load these
+        if (isset($data['always']))
+        {
+            $this->loadPsr4Group($data['always']);
+        }
+
+        // These only in admin
+        if (is_admin() && isset($data['admin']))
+        {
+            $this->loadPsr4Group($data['admin']);
+        }
+
+        // These only in frontend
+        if ( !is_admin() && isset($data['frontend']))
+        {
+            $this->loadPsr4Group($data['frontend']);
+        }
+    }
+
+    /**
+     * Automatically instanciate all the classes inside the instanciate sections for our groups
+     */
+    public function instantiateClasses()
+    {
+        $data = $this->getData();
+
+        // Always load these
+        if (isset($data['always']))
+        {
+            $this->instantiateGroup($data['always']);
+        }
+
+        // These only in admin
+        if (is_admin() && isset($data['admin']))
+        {
+            $this->instantiateGroup($data['admin']);
+        }
+
+        // These only in frontend
+        if ( !is_admin() && isset($data['frontend']))
+        {
+            $this->instantiateGroup($data['frontend']);
+        }
+    }
+
+    /**
+     * Autoload the classes for a group
+     *
+     * @param array $group The group
+     */
+    private function loadPsr4Group($group)
+    {
+        if ( !isset($group['psr4']))
+        {
+            return;
+        }
 
         /** @var \Composer\Autoload\ClassLoader $classLoader */
         $classLoader = Baobab::classLoader();
-        foreach ($data as $prefix => $paths)
+        foreach ($group['psr4'] as $prefix => $paths)
         {
             $classLoader->addPsr4($prefix, $paths);
+        }
+    }
+
+    /**
+     * Instantiate the classes for a group
+     *
+     * @param array $group The group
+     */
+    private function instantiateGroup($group)
+    {
+        if ( !isset($group['instantiate']))
+        {
+            return;
+        }
+
+        foreach ($group['instantiate'] as $className)
+        {
+            new $className();
         }
     }
 }
